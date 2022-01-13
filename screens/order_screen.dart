79a737fb86c0619bafc7fb7a0ff3aa24c828880f
Marcus <marcus.ng@agmostudio.com>
2022-01-13 16:07:51 +0800

    Added screens

---
+++ screens/order_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_example/controllers/order_controller.dart';
+import 'package:getx_example/widget/app_drawer.dart';
+import 'package:getx_example/widget/order_item.dart';
+
+class OrderScreen extends StatelessWidget {
+  var orderController = Get.put(OrderController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Yours Orders"),
+      ),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+          itemCount: orderController.orders.length,
+          itemBuilder: (context, index) =>
+              OrderItem(orderController.orders[index])),
+    );
+  }
+}
